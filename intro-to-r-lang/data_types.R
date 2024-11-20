454cf213bbd51c88964d70fdaa476afc87ca2b08
Burak Colak <burak.colak.yes@gmail.com>
2024-11-20 14:46:59 +0100

    Adding matrices ,lists ...

--- intro-to-r-lang/data_types.R
+++ intro-to-r-lang/data_types.R
@@ -37,6 +37,7 @@ v1 <- vector(mode = "integer", length = 100)  # A vector of 100 integers
 # Generally, all numbers in R are treated as numeric objects (double precision real numbers).
 # If you want to create an integer, use the `L` suffix:
 int_example <- 12L  # This creates 12 as an integer
+typeof(int_example) # > "integer"
 
 # Special numeric values:
 # - `Inf` represents infinity. For example, 1/0 gives Inf.
@@ -66,3 +67,67 @@ x <- c(0.5, 0.2)
 
 # Creating a numeric vector of specified length:
 y <- vector("numeric", length = 10)  # A numeric vector of length 10, filled with 0s
+
+# If you provide multiple diff type to this vector creation it takes the commons:
+# Via Conversion
+vec_w_diff_types = c(1.2, 'burak') #> this is vec of chars
+
+
+# =======================
+#        Coercion (as.*)
+# =======================
+x <- 0:6
+class(x) # Integer
+as.numeric(x)
+as.logical(x)
+as.character(x)
+as.complex(x)
+
+# This not all works:
+x <- c("a", "b", "c")
+as.numeric(x) #> NA NA NA
+
+
+# =======================
+#        Lists 
+# =======================
+# special type of vectors
+# Like a vector but can contain of different classes, numeric, char etc together
+
+x <- list(1, "a", TRUE, 1+4i)
+
+# Element of lists has double brackets where vectors has single bracket.
+
+
+# =======================
+#        Matrix 
+# =======================
+# vectors with dimension attribute
+# dimension attribute is itself an integer vector of length2 (nrow, ncol)
+
+m <- matrix(nrow = 2, ncol = 3)
+
+dim(m)
+attributes(m)
+
+# Matrices fill up by columns, you fill 1st col then 2nd ... nth col.
+m <- matrix(1:6, nrow = 2, ncol = 3)
+# > m
+#     [,1] [,2] [,3]
+# [1,]    1    3    5
+# [2,]    2    4    6
+
+
+# Matrices can be created also by adding the dimmension attrbs to vecs
+v <- 1:10
+dim(v) <- c(2,5)
+# > v <- 1:10
+# > v
+# [1]  1  2  3  4  5  6  7  8  9 10
+# > dim(v) <- c(2,5)
+# > v
+#     [,1] [,2] [,3] [,4] [,5]
+# [1,]    1    3    5    7    9
+# [2,]    2    4    6    8   10
+
+# Another way is Binding cols or binding rows `cbin()` & `rbind()`
